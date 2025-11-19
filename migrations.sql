130d042531005f1aaabb08ed37b803254ee60479
Dhanalakshmi M <109088943+Dhanalakshmi2002@users.noreply.github.com>
2025-11-19 11:08:05 +0530

    Create links table with unique index

---
+++ migrations.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS links (
+  code VARCHAR(8) PRIMARY KEY,
+  target_url TEXT NOT NULL,
+  clicks INTEGER NOT NULL DEFAULT 0,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  last_clicked TIMESTAMP WITH TIME ZONE
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS links_code_idx ON links(code);
